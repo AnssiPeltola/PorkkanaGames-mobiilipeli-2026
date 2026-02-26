e7ee0ed163d845f2942f8dc942bbe956555c8a74
Anssi Peltola <anssipeltola@hotmail.com>
2026-02-26 22:59:04 +0200

    Level Three Proto
    New Scene LevelThree
    - Walls with collisionShape2D to block CookedIngredients to move out of game.
    - Handmade good looking Plate as Sprite2D in background.
    - Area2D Pasta/Sauce
      - Has half transparent Sprite2D to demonstrate where pasta/sauce should be moved and collisionShape2D that does nothing for now.
    - CharacterBody2D CookedIngredient
      - Uses same nodes than all other ingredients, but it has 2 different CollisionShape2Ds if its either pasta or sauce. Both CollisionShape2Ds are disabled from editor and in code enabled by group.
      - Uses new C# Script CookedIngredient.cs
        - In _Ready function we load textures for pasta/sauce and load CollisionShapes in variables
        - If this CookedIngredient object is in group "Pasta" or "Sauce" changes Sprite2D texture by that.
        - And by group it will make CollisionShape2D from disabled to "Enabled" by this code ´_pastaCollision.Disabled = false; ´
    
    Art
    - Handmade pretty pictures of plate, sauce and pasta.

---
+++ porkkana-games-peli/Code/CookedIngredient/CookedIngredient.cs
@@ -0,0 +1,77 @@
+using Godot;
+
+public partial class CookedIngredient : CharacterBody2D
+{
+	private bool _dragging = false;
+	[Export] private int _clickRadius = 32;
+	private Sprite2D _sprite;
+	private Texture2D _pastaTexture;
+	private Texture2D _sauceTexture;
+	private CollisionShape2D _pastaCollision;
+	private CollisionShape2D _sauceCollision;
+
+    public override void _Ready()
+    {
+		// We get this scenes Sprite2D node in variable _sprite
+        _sprite = GetNode<Sprite2D>("Sprite2D");
+
+		// Pasta and sauce Sprite2D Load
+		_pastaTexture = GD.Load<Texture2D>("res://Art/Examples/pasta.png");
+		_sauceTexture = GD.Load<Texture2D>("res://Art/Examples/tomatosauce.png");
+
+		_pastaCollision = GetNode<CollisionShape2D>("PastaCollision");
+		_sauceCollision = GetNode<CollisionShape2D>("SauceCollision");
+
+		// Change Sprite2D Texture and Enable CollisionShape2D by group
+		if (this.IsInGroup("Pasta"))
+		{
+			ChangeSprite(_pastaTexture);
+			_pastaCollision.Disabled = false;
+
+		}
+
+		if (this.IsInGroup("Sauce"))
+		{
+			ChangeSprite(_sauceTexture);
+			_sauceCollision.Disabled = false;
+
+		}
+    }
+
+	// This function is called for every input event (mouse, keyboard, touch, etc.)
+   public override void _Input(InputEvent e)
+    {
+		// Only react to screen touch events (mobile / mouse click)
+        if (e is InputEventScreenTouch touch)
+        {
+			// Check if the touch is close enough to this object to start dragging and set _dragging true
+			if ((touch.Position - GlobalPosition).Length() < _clickRadius)
+			{
+            	_dragging = touch.Pressed;
+			}
+        }
+    }
+
+	// if dragging false does nothing
+	public override void _PhysicsProcess(double delta)
+    {
+        if (!_dragging)
+		{
+			return;
+		}
+
+		// Get the current position of finger/mouse
+        Vector2 target = GetGlobalMousePosition();
+        Vector2 direction = target - GlobalPosition;
+
+        Velocity = direction / (float)delta;
+		// Makes the move using Godot physics
+        MoveAndSlide();
+    }
+
+	// Function that changes this scenes Sprite2D texture to new
+	public void ChangeSprite(Texture2D newTexture)
+	{
+		_sprite.Texture = newTexture;
+	}
+}

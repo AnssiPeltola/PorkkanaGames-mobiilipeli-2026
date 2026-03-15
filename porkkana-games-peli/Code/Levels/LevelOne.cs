a690f4e6a5f37a2a87928918baf3094eec56d90d
pcArch <PcArchEmail>
2026-03-15 13:55:11 +0200

    edit: LevelOne.cs
    Added comments on the testing sequence

--- porkkana-games-peli/Code/Levels/LevelOne.cs
+++ porkkana-games-peli/Code/Levels/LevelOne.cs
@@ -7,11 +7,12 @@ public partial class LevelOne : BaseLevel
 	public override void _Ready()
 	{
 		CurrentLevel = 1;
-
 	}
 
 	public override void _Process(double delta)
 	{
+		// GODOT Spacbar is default assigned to ui_select & ui_accept
+		// So to test press spacebar to run scene changing sequence
 		if (Input.IsActionJustPressed("ui_select"))
 		{
 			CurrentComplete();

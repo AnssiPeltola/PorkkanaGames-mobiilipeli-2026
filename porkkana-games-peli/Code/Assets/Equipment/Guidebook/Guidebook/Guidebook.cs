2104ad0bd51b355c7b5ba5f0e4a17d3804fc242c
Anssi Peltola <anssipeltola@hotmail.com>
2026-03-22 21:55:34 +0200

    Guidebook for levels
    Added guidebook into every level and added [Export] public Texture2D GuideBookPageTexture; into Guidebook.cs so the pages sprite2Ds texture can be set from editor in each level its own.

--- porkkana-games-peli/Code/Assets/Equipment/Guidebook/Guidebook/Guidebook.cs
+++ porkkana-games-peli/Code/Assets/Equipment/Guidebook/Guidebook/Guidebook.cs
@@ -3,9 +3,17 @@ using System;
 
 public partial class Guidebook : Control
 {
+	[Export] public Texture2D GuideBookPageTexture;
+	 private Sprite2D _sprite;
+
 	// On load make this guidebook invisible
 	public override void _Ready()
 	{
 		Visible = false;
+		_sprite = GetNodeOrNull<Sprite2D>("Sprite2D");
+        if (_sprite != null && GuideBookPageTexture != null)
+        {
+            _sprite.Texture = GuideBookPageTexture;
+        }
 	}
 }

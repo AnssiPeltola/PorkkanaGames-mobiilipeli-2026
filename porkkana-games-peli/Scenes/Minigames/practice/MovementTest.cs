bf8dd257b973d6cda82de83d8d397aaab1dd16d0
pcArch <PcArchEmail>
2026-03-30 13:06:01 +0300

    Cleaning up, Distance tracking
    Cleaning upp the comments and code look
    NEW: Distance track, we claculate the distance between two points

--- porkkana-games-peli/Scenes/Minigames/practice/MovementTest.cs
+++ porkkana-games-peli/Scenes/Minigames/practice/MovementTest.cs
@@ -29,16 +29,19 @@ public partial class MovementTest : Node2D
 	// trach any type of input
 	public override void _Input(InputEvent e)
 	{
-		// if input is touch screen
+		// Touch input tracking
 		if (e is InputEventScreenTouch touch)
 		{
 			// Touch input start
+			// If toch then ->>
 			if (touch.Pressed)
 			{
 				GD.Print("Touch @: " + touch.Position);
 				_sprite.Position = touch.Position;
+
 				// 2.
 				_vectorStart = touch.Position;
+
 				// Change color
 				_sprite.Modulate = new Color(1, 0, 0); // Red when touching
 			}
@@ -49,30 +52,24 @@ public partial class MovementTest : Node2D
 				// Vector end position
 				_vectorEnd = touch.Position;
 				GD.Print("Touch END @: " + touch.Position);
+
 				// Change color
 				_sprite.Modulate = new Color(1, 1, 1); // White when released
+				 
+				// Print _vectorStart location constantly
+				GD.Print($"New Vector End: {_vectorEnd}");
 
 				// 3. Calculate drag distance
 				float distance = _vectorStart.DistanceTo(touch.Position);
+				GD.Print($"Distance was {distance}");
 			}
 		}
-		// Drag tracking
+		// Drag input tracking
 		// If drag then ->>
 		if (e is InputEventScreenDrag drag)
 		{
 			GD.Print("Dragging to: " + drag.Position);
-			 // TEST:
-			// Add a delay / drag for the Sprite2D
-			// _sprite.Position = drag.Position.Lerp(drag.Position, 0.3f);
 			_sprite.Position = drag.Position;
-			// 2.
-			// _vectorEnd = drag.Position;
 		}
-
-		 // Print _vectorStart location constantly
-		GD.Print($"Vector Start: {_vectorStart}");
-		
-		// Print _vectorStart location constantly
-		GD.Print($"Vector End: {_vectorEnd}");
 	}
 }

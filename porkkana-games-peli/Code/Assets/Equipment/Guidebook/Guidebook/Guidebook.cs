da5739915f4c866041ed4f107bdd916e7f4eb919
Anssi Peltola <anssipeltola@hotmail.com>
2026-04-08 19:15:31 +0300

    GuidebookPages
    Refactored guidebook.cs.
    
    It has now 2x sprite2Ds for guide pages and both can set from editor.
    
    Added new textures for each recipe and level for guidebook pages

--- porkkana-games-peli/Code/Assets/Equipment/Guidebook/Guidebook/Guidebook.cs
+++ porkkana-games-peli/Code/Assets/Equipment/Guidebook/Guidebook/Guidebook.cs
@@ -3,17 +3,27 @@ using System;
 
 public partial class Guidebook : Control
 {
-	[Export] public Texture2D GuideBookPageTexture;
-	 private Sprite2D _sprite;
+	[Export] public Texture2D PageOneTexture;
+	[Export] public Texture2D PageTwoTexture;
+
+	 private Sprite2D _pageOne;
+	 private Sprite2D _pageTwo;
 
 	// On load make this guidebook invisible
 	public override void _Ready()
 	{
 		Visible = false;
-		_sprite = GetNodeOrNull<Sprite2D>("Sprite2D");
-        if (_sprite != null && GuideBookPageTexture != null)
+		_pageOne = GetNodeOrNull<Sprite2D>("PageOne");
+		_pageTwo = GetNodeOrNull<Sprite2D>("PageTwo");
+
+        if (_pageOne != null && PageOneTexture != null)
         {
-            _sprite.Texture = GuideBookPageTexture;
+            _pageOne.Texture = PageOneTexture;
+        }
+
+		if (_pageTwo != null && PageTwoTexture != null)
+        {
+            _pageTwo.Texture = PageTwoTexture;
         }
 	}
 }

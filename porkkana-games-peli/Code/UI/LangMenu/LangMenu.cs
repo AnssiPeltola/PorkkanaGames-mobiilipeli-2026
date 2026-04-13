6d5bc708d537b020502ff9127052e425ff2dc5be
Anssi Peltola <anssipeltola@hotmail.com>
2026-04-13 22:25:11 +0300

    PauseMenu Modifications
    Made pauseMenu look "better", by sizing buttons and adding gap between them.
    
    Fixed abit langMenu scene by deleting CanvasLayer that were ruining the usage in other scenes. It forced it to upcorner and couldnt move it.
    
    Added fade animation to langmenu like the other pausemenu has.

--- porkkana-games-peli/Code/UI/LangMenu/LangMenu.cs
+++ porkkana-games-peli/Code/UI/LangMenu/LangMenu.cs
@@ -5,9 +5,9 @@ public partial class LangMenu : Control
 {
 	public override void _Ready()
 	{
-		GetNode<TextureButton>("CanvasLayer/LangButtons/LangFi").Pressed += OnLangFiPressed;
-		GetNode<TextureButton>("CanvasLayer/LangButtons/LangEng").Pressed += OnLangEngPressed;
-		GetNode<TextureButton>("CanvasLayer/LangButtons/LangSwe").Pressed += OnLangSwePressed;
+		GetNode<TextureButton>("LangButtons/LangFi").Pressed += OnLangFiPressed;
+		GetNode<TextureButton>("LangButtons/LangEng").Pressed += OnLangEngPressed;
+		GetNode<TextureButton>("LangButtons/LangSwe").Pressed += OnLangSwePressed;
 	}
 
 	private void OnLangEngPressed()
